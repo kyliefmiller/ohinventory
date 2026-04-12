4e0af64d4ac68b78d83e2bb09f80fadd89f3aad0
kyliefmiller <kylie.f.miller@ucdenver.edu>
2026-04-12 13:25:11 -0600

    info update again

--- database/models.py
+++ database/models.py
@@ -7,11 +7,11 @@ class OHEntry(models.Model):
     year = models.IntegerField(null=True, blank=True)
     month = models.CharField(max_length=1000, null=True, blank=True)
     day = models.IntegerField(null=True, blank=True)
-    narrator = models.CharField(max_length=100, null=True, blank=True)
+    narrator = models.CharField(max_length=1000, null=True, blank=True)
     creator = models.CharField(max_length=100, null=True, blank=True)
     location = models.CharField(max_length=1000, null=True, blank=True)
     topic = models.CharField(max_length=1000, null=True, blank=True)
     description = models.CharField(max_length=1000000, null=True, blank=True)
     link = models.CharField(max_length=1000, null=True, blank=True)
-    date_updated = models.CharField(max_length=100, default="April 12, 2026")
+    date_updated = models.CharField(max_length=100)
 
