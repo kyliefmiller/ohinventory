af3dfaa6fdfbbbd5320e725c267190644a611b3a
kyliefmiller <kylie.f.miller@ucdenver.edu>
2026-04-12 12:20:30 -0600

    date updated

--- database/models.py
+++ database/models.py
@@ -13,4 +13,5 @@ class OHEntry(models.Model):
     topic = models.CharField(max_length=1000, null=True, blank=True)
     description = models.CharField(max_length=1000000, null=True, blank=True)
     link = models.CharField(max_length=1000, null=True, blank=True)
+    date_updated = models.CharField(max_length=100, default="April 12, 2026")
 
